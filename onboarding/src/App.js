ee6f40536c2831fac6efde87ef1048f1477879d1
Jose Carlos Robles <jcrobles1989@gmail.com>
2020-03-26 14:16:22 -0700

    finished

--- onboarding/src/App.js
+++ onboarding/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Form from './Components/Form';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
+      <h1>User Onboarding</h1>
       <Form />
     </div>
   );
 }
-
-export default App;
